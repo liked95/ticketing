a9886f6c2f07d340e583d1d0629749e9dc51cb21
sonvh <sonvh@bravebits.vn>
2024-12-28 10:49:29 +0700

    Update mongo uri to env

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,13 +7,17 @@ const start = async () => {
       throw new Error("JWT_KEY must be defined!");
     }
 
-    await moogoose.connect("mongodb://auth-mongo-srv:27017/auth", {});
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI must be defined!");
+    }
+
+    await moogoose.connect(process.env.MONGO_URI, {});
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error(error);
   }
 
-  app.listen(3000, () => console.log("Auth service is running on 3000"));
+  app.listen(3000, () => console.log("Auth service is running on 3000!!!"));
 };
 
 start();
