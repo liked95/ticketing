3ea1eb35cc56d5c32939a9123a482a0985e6cbae
sonvh <sonvh@bravebits.vn>
2025-01-06 21:38:46 +0700

    Implement TicketCreatedListener and TicketUpdatedListener for event handling; update ticket model to include id

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -1,12 +1,16 @@
-import { Message } from "node-nats-streaming";
-import { Subjects, Listener, TicketCreatedEvent } from "@sonnytickets/common";
-import { queueGroupName } from "./queue-group-name";
+import {Message} from 'node-nats-streaming'
+import {Subjects, Listener, TicketCreatedEvent} from '@sonnytickets/common'
+import {queueGroupName} from './queue-group-name'
+import {Ticket} from '../../models/ticket'
 
-export class TicketCreatedListener extends Listener<TicketCreatedEvent>{
-    subject: Subjects.TicketCreated = Subjects.TicketCreated;
-    queueGroupName: string = queueGroupName
-    onMessage(data: TicketCreatedEvent['data'], msg: Message) {
-        
-    }
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated
+  queueGroupName: string = queueGroupName
+  async onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+    const {id, title, price} = data
+    const ticket = Ticket.build({id, title, price})
+    await ticket.save()
 
+    msg.ack()
+  }
 }
