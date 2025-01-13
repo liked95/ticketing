be715df045f4fc41a0acf6ec5515a809de6dca45
sonvh <sonvh@bravebits.vn>
2025-01-13 16:59:32 +0700

    auth v4

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import moogoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
-  console.log('Starting auth server v3...')
+  console.log('Starting auth server v4...')
   try {
     if (!process.env.JWT_KEY) {
       throw new Error("JWT_KEY must be defined!");
