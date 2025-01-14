72a049e4997d7fc723883b2d248596e80ba865b7
sonvh <sonvh@bravebits.vn>
2025-01-14 16:29:06 +0700

    Logg

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export default ({ req }) => {
+    console.log('✅✅✅✅ sonny baseurl', process.env.CLIENT_BASE_URL)
     if (typeof window === 'undefined') {
         return axios.create({
             baseURL: process.env.CLIENT_BASE_URL,
